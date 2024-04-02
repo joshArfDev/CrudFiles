0e13190ffde5b69ba6df517b3650e874a5e9d2eb
joshArfDev <soondevjosh@gmail.com>
2024-04-02 10:58:45 -0600

    agregando comentarios

--- src/com/guatex/productos/app/ComGuatexProductosApp.java
+++ src/com/guatex/productos/app/ComGuatexProductosApp.java
@@ -25,9 +25,11 @@ public class ComGuatexProductosApp {
 
     static Scanner scanner = new Scanner(System.in); //para entrada de datos en consola
     static LinkedList<EProducto> productos = new LinkedList<>(); // Creando nuestra lista enlazada
-    static String archivo = "productos.txt"; // nombre de nuestro archivo
+    static String nombreArchivo = "C:\\Users\\GUATEX\\Documents\\NetBeansProjects\\com.guatex.productos.app\\src\\com\\guatex\\productos\\Datos\\productos.txt"; 
+    static String archivo = nombreArchivo ; // nombre de nuestro archivo
 
     public static void main(String[] args) {
+
         int opcion = 0;
 
         cargarDatos();
@@ -85,20 +87,6 @@ public class ComGuatexProductosApp {
         scanner.close();
     }
 
-    public static void listarDatos() {
-
-        System.out.println("Lista de productos ingresados:");
-
-        for (EProducto producto : productos) {
-            System.out.println("Nombre: " + producto.getNombreProducto());
-            System.out.println("Descripción: " + producto.getDescripcionProducto());
-            System.out.println("Precio en bodega: " + producto.getPrecioBodega());
-            System.out.println("Precio de venta: " + producto.getPrecioVenta());
-            System.out.println("Cantidad: " + producto.getCantidad());
-            System.out.println("------------------------");
-        }
-    }
-
     public static void insertaDatos() {
 
         System.out.println("Ingrese el nombre del producto: ");
@@ -116,6 +104,7 @@ public class ComGuatexProductosApp {
         System.out.println("Ingrese la cantidad del producto: ");
         int cantidad = Integer.parseInt(scanner.nextLine());
 
+        //Creando objeto para producto que ingresemos
         EProducto producto = new EProducto();
         producto.setNombreProducto(nombre);
         producto.setDescripcionProducto(descripcion);
@@ -123,22 +112,22 @@ public class ComGuatexProductosApp {
         producto.setPrecioVenta(precioVenta);
         producto.setCantidad(cantidad);
 
+        //Añadimos nuestro objeto producto a nuetro linkedlist
         productos.add(producto);
     }
 
-    public static void guardarDatos() {
-        try (FileWriter writer = new FileWriter(archivo)) {
-            for (EProducto producto : productos) {
-                String data = producto.getNombreProducto() + ","
-                        + producto.getDescripcionProducto() + ","
-                        + producto.getPrecioBodega() + ","
-                        + producto.getPrecioVenta() + ","
-                        + producto.getCantidad() + "\n";
-                writer.write(data);
-            }
-            System.out.println("Datos guardados correctamente en " + archivo);
-        } catch (IOException e) {
-            System.out.println("Error al guardar los datos: " + e.getMessage());
+    public static void listarDatos() {
+
+        System.out.println("Lista de productos ingresados:");
+
+        //Recorremos nuestra linkedlist, gracias a la variables producto (esta variable es de tipo " nuestro Objeto EProducto" )
+        for (EProducto producto : productos) {
+            System.out.println("Nombre: " + producto.getNombreProducto());
+            System.out.println("Descripción: " + producto.getDescripcionProducto());
+            System.out.println("Precio en bodega: " + producto.getPrecioBodega());
+            System.out.println("Precio de venta: " + producto.getPrecioVenta());
+            System.out.println("Cantidad: " + producto.getCantidad());
+            System.out.println("------------------------");
         }
     }
 
@@ -147,8 +136,10 @@ public class ComGuatexProductosApp {
         System.out.println("Ingrese producto a buscar: ");
         String productoBuscar = scanner.nextLine();
 
+        //Bandera para poder hacer la validación de la existencia del producto
         boolean encontrado = false;
 
+        //Recorremos nuestra linkedlist y obtenemos (get) los datos del producto solo si coincide lo que estamos buscando
         for (EProducto producto : productos) {
             if (producto.getNombreProducto().equalsIgnoreCase(productoBuscar)) {
                 System.out.println("\nProducto encontrado:\n");
@@ -167,38 +158,17 @@ public class ComGuatexProductosApp {
         }
     }
 
-    public static void cargarDatos() {
-
-        try (Scanner fileScanner = new Scanner(new File(archivo))) {
-
-            while (fileScanner.hasNextLine()) {
-                String dataProducto = fileScanner.nextLine();
-                String[] posicion = dataProducto.split(",");
-                if (posicion.length == 5) {
-                    EProducto producto = new EProducto();
-                    producto.setNombreProducto(posicion[0]);
-                    producto.setDescripcionProducto(posicion[1]);
-                    producto.setPrecioBodega(Float.parseFloat(posicion[2]));
-                    producto.setPrecioVenta(Float.parseFloat(posicion[3]));
-                    producto.setCantidad(Integer.parseInt(posicion[4]));
-                    productos.add(producto);
-                }
-            }
-            System.out.println("Datos cargados correctamente desde " + archivo);
-        } catch (FileNotFoundException e) {
-            System.out.println("No se encontró el archivo " + archivo);
-        }
-
-    }
-
     public static void actualizarDato() {
 
         System.out.println("Producto a actualizar: ");
         String actualizarProducto = scanner.nextLine();
 
+        //Bandera para poder hacer la validación de la existencia del producto
         boolean encontrado = false;
 
+        //Recorremos nuestra linkedlist y actualizamos (set) los datos del producto solo si coincide lo que estamos buscando
         for (EProducto producto : productos) {
+
             if (producto.getNombreProducto().equalsIgnoreCase(actualizarProducto)) {
                 encontrado = true;
 
@@ -237,8 +207,10 @@ public class ComGuatexProductosApp {
         System.out.println("Producto a eliminar: ");
         String eliminarProducto = scanner.nextLine();
 
+        //Bandera para poder hacer la validación de la existencia del producto
         boolean encontrado = false;
 
+        //Recorremos nuestra linkedlist y obtenemos (get) los datos del producto solo si coincide lo que estamos buscando
         for (EProducto producto : productos) {
             if (producto.getNombreProducto().equalsIgnoreCase(eliminarProducto)) {
                 System.out.println("\nProducto encontrado:\n");
@@ -248,6 +220,7 @@ public class ComGuatexProductosApp {
                 System.out.println("Precio de venta: " + producto.getPrecioVenta());
                 System.out.println("Cantidad: " + producto.getCantidad());
                 encontrado = true;
+                //eliminanos el producto
                 productos.remove(producto);
                 break;
             }
@@ -258,6 +231,55 @@ public class ComGuatexProductosApp {
         }
 
         System.out.println("\n producto eliminado");
+
+    }
+
+    public static void guardarDatos() {
+        // Intentamos escribir en nuestro archivo
+        try (FileWriter writer = new FileWriter(archivo)) {
+            // Recorremos nuestra linkedlist y obtenemos (get) los datos de cada producto 
+            for (EProducto producto : productos) {
+                String data = producto.getNombreProducto() + ","
+                        + producto.getDescripcionProducto() + ","
+                        + producto.getPrecioBodega() + ","
+                        + producto.getPrecioVenta() + ","
+                        + producto.getCantidad() + "\n";
+                // Escribimos en el archivo
+                writer.write(data);
+            }
+            System.out.println("Datos guardados correctamente en " + archivo);
+        } catch (IOException e) {
+            System.out.println("Error al guardar los datos: " + e.getMessage());
+        }
+    }
+
+    public static void cargarDatos() {
+
+        //Intentamos escanear (leer) nuestro archivo
+        try (Scanner fileScanner = new Scanner(new File(archivo))) {
+
+            while (fileScanner.hasNextLine()) {
+                // Se lee la línea actual y se almacena en la variable dataProducto
+                String dataProducto = fileScanner.nextLine();
+                // Se verifica que la línea contenga los 5 elementos esperados (nombre, descripción, precio bodega, precio venta y cantidad)
+                String[] posicion = dataProducto.split(",");
+                // Si tenemos los 5 espacios inicializamos nuestro objeto e insertamos (set) los datos
+                if (posicion.length == 5) {
+                    EProducto producto = new EProducto();
+                    producto.setNombreProducto(posicion[0]);
+                    producto.setDescripcionProducto(posicion[1]);
+                    producto.setPrecioBodega(Float.parseFloat(posicion[2]));
+                    producto.setPrecioVenta(Float.parseFloat(posicion[3]));
+                    producto.setCantidad(Integer.parseInt(posicion[4]));
+
+                    //Agregamos el producto a nuestra linkedlist
+                    productos.add(producto);
+                }
+            }
+            System.out.println("Datos cargados correctamente desde " + archivo);
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontró el archivo " + archivo);
+        }
 
     }
 
